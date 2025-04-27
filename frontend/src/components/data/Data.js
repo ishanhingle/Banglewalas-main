d57b8c142a1b630f6feedd0451a7b6318f5a31c2
ishanhingle <134205529+ishanhingle@users.noreply.github.com>
2025-04-27 20:23:38 +0530

    Initial commit

---
+++ frontend/src/components/data/Data.js
@@ -0,0 +1,381 @@
+export const nav = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "about",
+    path: "/about",
+  },
+  {
+    text: "services",
+    path: "/services",
+  },
+  {
+    text: "blog",
+    path: "/blog",
+  },
+  {
+    text: "pricing",
+    path: "/pricing",
+  },
+  {
+    text: "contact",
+    path: "/contact",
+  },
+  {
+    text: "Upload Property",
+    path:  "/upload",
+  }
+];
+export const featured = [
+  {
+    cover: "../images/hero/h1.png",
+    name: "Family House",
+    total: "122 Property",
+  },
+  {
+    cover: "../images/hero/h2.png",
+    name: "House & Villa",
+    total: "155 Property",
+  },
+  {
+    cover: "../images/hero/h3.png",
+    name: "Apartment",
+    total: "300 Property",
+  },
+  {
+    cover: "../images/hero/h4.png",
+    name: "Office & Studio",
+    total: "80 Property",
+  },
+  {
+    cover: "../images/hero/h6.png",
+    name: "Villa & Condo",
+    total: "80 Property",
+  },
+];
+export const list = [
+  {
+    id: 1,
+    cover: "../images/list/p-1.png",
+    name: "Red Carpet Real Estate",
+    location: "12/4, Vijay Nagar, Near Meghdoot Garden",
+    category: "For Rent",
+    price: "₹3,700",
+    type: "Apartment",
+  },
+  {
+    id: 2,
+    cover: "../images/list/p-2.png",
+    name: "Fairmount Properties",
+    location: "B-203, Silver Heights Apartments, Palasia Square",
+    category: "For Sale",
+    price: "₹9,750",
+    type: "Condos",
+  },
+  {
+    id: 3,
+    cover: "../images/list/p-7.png",
+    name: "The Real Estate Corner",
+    location: "45, Scheme No. 78, Part 2, Opp. C21 Mall",
+    category: "For Rent",
+    price: "₹5,860",
+    type: "Offices",
+  },
+  {
+    id: 4,
+    cover: "../images/list/p-4.png",
+    name: "Herringbone Realty",
+    location: "118, Sudama Nagar, Sector D",
+    category: "For Sale",
+    price: "₹7,540",
+    type: "Homes & Villas",
+  },
+  {
+    id: 5,
+    cover: "../images/list/p-5.png",
+    name: "Brick Lane Realty",
+    location: "7-A, Bhawarkuan Road, Near GDC College",
+    category: "For Rent",
+    price: "₹4,850",
+    type: "Commercial",
+  },
+  {
+    id: 6,
+    cover: "../images/list/p-6.png",
+    name: "Banyon Tree Realty",
+    location: "33, Rajendra Nagar, Sector C, Behind SICA School",
+    category: "For Sale",
+    price: "₹2,742",
+    type: "Apartment",
+  },
+];
+export const awards = [
+  {
+    icon: <i class="fa-solid fa-trophy"></i>,
+    num: "32 M	",
+    name: "Blue Burmin Award",
+  },
+  {
+    icon: <i class="fa-solid fa-briefcase"></i>,
+    num: "43 M",
+    name: "Mimo X11 Award",
+  },
+  {
+    icon: <i class="fa-solid fa-lightbulb"></i>,
+    num: "51 M",
+    name: "Australian UGC Award",
+  },
+  {
+    icon: <i class="fa-solid fa-heart"></i>,
+    num: "42 M",
+    name: "IITCA Green Award",
+  },
+];
+export const location = [
+  {
+    id: 1,
+    name: "Indore",
+    Villas: "12 Villas",
+    Apartments: "10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/Indore.jpg",
+  },
+  {
+    id: 2,
+    name: "Bhopal",
+    Villas: "12 Villas",
+    Apartments: "10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/Bhopal.jpg",
+  },
+  {
+    id: 3,
+    name: "Ujjain",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/Ujjain.jpg",
+  },
+  {
+    id: 4,
+    name: "Gwalior",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/Gwalior.jpg",
+  },
+  {
+    id: 5,
+    name: "Jabalpur",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/Jabalpur.jpg",
+  },
+  {
+    id: 6,
+    name: "Dewas",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/Dewas.jpg",
+  },
+];
+export const team = [
+  {
+    list: "50",
+    cover: "../images/customer/team-1.jpg",
+    address: "Indore",
+    name: "Sargam S. Singh",
+    icon: [
+      <i class="fa-brands fa-facebook-f"></i>,
+      <i class="fa-brands fa-linkedin"></i>,
+      <i class="fa-brands fa-twitter"></i>,
+      <i class="fa-brands fa-instagram"></i>,
+    ],
+  },
+  {
+    list: "70",
+    cover: "../images/customer/team-2.jpg",
+    address: "Ujjain",
+    name: "Harijeet M. Siller",
+    icon: [
+      <i class="fa-brands fa-facebook-f"></i>,
+      <i class="fa-brands fa-linkedin"></i>,
+      <i class="fa-brands fa-twitter"></i>,
+      <i class="fa-brands fa-instagram"></i>,
+    ],
+  },
+  {
+    list: "80",
+    cover: "../images/customer/team-3.jpg",
+    address: "Bhopal",
+    name: "Ashish K. Jha",
+    icon: [
+      <i class="fa-brands fa-facebook-f"></i>,
+      <i class="fa-brands fa-linkedin"></i>,
+      <i class="fa-brands fa-twitter"></i>,
+      <i class="fa-brands fa-instagram"></i>,
+    ],
+  },
+  {
+    list: "51",
+    cover: "../images/customer/team-4.jpg",
+    address: "Jabalpur",
+    name: "Anita Patidar",
+    icon: [
+      <i class="fa-brands fa-facebook-f"></i>,
+      <i class="fa-brands fa-linkedin"></i>,
+      <i class="fa-brands fa-twitter"></i>,
+      <i class="fa-brands fa-instagram"></i>,
+    ],
+  },
+  {
+    list: "42",
+    cover: "../images/customer/team-5.jpg",
+    address: "Gwalior",
+    name: "David Fernandez",
+    icon: [
+      <i class="fa-brands fa-facebook-f"></i>,
+      <i class="fa-brands fa-linkedin"></i>,
+      <i class="fa-brands fa-twitter"></i>,
+      <i class="fa-brands fa-instagram"></i>,
+    ],
+  },
+  {
+    list: "38",
+    cover: "../images/customer/team-6.jpg",
+    address: "Dewas",
+    name: "Rohan Patel",
+    icon: [
+      <i class="fa-brands fa-facebook-f"></i>,
+      <i class="fa-brands fa-linkedin"></i>,
+      <i class="fa-brands fa-twitter"></i>,
+      <i class="fa-brands fa-instagram"></i>,
+    ],
+  },
+];
+export const price = [
+  {
+    plan: "Basic",
+    price: "29",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class="fa-solid fa-check"></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class="fa-solid fa-check"></i>,
+        text: "120GB CDN Bandwidth",
+      },
+      {
+        icon: <i class="fa-solid fa-check"></i>,
+        text: "5GB Cloud Storage",
+      },
+      {
+        change: "color",
+        icon: <i class="fa-solid fa-x"></i>,
+        text: "Personal Help Support",
+      },
+      {
+        change: "color",
+        icon: <i class="fa-solid fa-x"></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+  {
+    best: "Best Value",
+    plan: "Standard",
+    price: "49",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class="fa-solid fa-check"></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class="fa-solid fa-check"></i>,
+        text: "150GB CDN Bandwidth",
+      },
+      {
+        icon: <i class="fa-solid fa-check"></i>,
+        text: "10GB Cloud Storage",
+      },
+      {
+        icon: <i class="fa-solid fa-check"></i>,
+        text: "Personal Help Support",
+      },
+      {
+        change: "color",
+        icon: <i class="fa-solid fa-x"></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+  {
+    plan: "Platinum",
+    price: "79",
+    ptext: "2 user, per month",
+    list: [
+      {
+        icon: <i class="fa-solid fa-check"></i>,
+        text: "100% Uptime Guarantee",
+      },
+      {
+        icon: <i class="fa-solid fa-check"></i>,
+        text: "200GB CDN Bandwidth",
+      },
+      {
+        icon: <i class="fa-solid fa-check"></i>,
+        text: "20GB Cloud Storage",
+      },
+      {
+        icon: <i class="fa-solid fa-check"></i>,
+        text: "Personal Help Support",
+      },
+      {
+        icon: <i class="fa-solid fa-check"></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+];
+export const footer = [
+  {
+    title: "LAYOUTS",
+    text: [
+      { list: "Home Page" },
+      { list: "About Page" },
+      { list: "Service Page" },
+      { list: "Property Page" },
+      { list: "Contact Page" },
+      { list: "Single Blog" },
+    ],
+  },
+  {
+    title: "ALL SECTIONS",
+    text: [
+      { list: "Headers" },
+      { list: "Features" },
+      { list: "Attractive" },
+      { list: "Testimonials" },
+      { list: "Videos" },
+      { list: "Footers" },
+    ],
+  },
+  {
+    title: "COMPANY",
+    text: [
+      { list: "About" },
+      { list: "Blog" },
+      { list: "Pricing" },
+      { list: "Affiliate" },
+      { list: "Login" },
+      { list: "Changelog" },
+    ],
+  },
+];
